4660a462f0913a1bd5650292f527ac778ba06f04
MustapheMS <moztaphem9@gmail.com>
2023-05-22 10:12:38 +0300

    Largest of three Numbers Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+    let x = 7;
+    let y = 14;
+    let z = 4;
+
+
+    if (x > y && x > z) {
+  console.log("The largest number out of the three numbers is x.");
+} else if (y > x && y > z) {
+  console.log("The largest number out of the three numbers is y.");
+} else if (z > x && z > y) {
+  console.log("The largest number out of the three numbers is z.");
+} else {
+  console.log("Two or more numbers are equal.");
+}
+
